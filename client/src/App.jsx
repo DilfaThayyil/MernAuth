9b26a11ab876cdce8140cf75f36b42319a369245
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-27 15:03:50 +0530

    react-router-dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+
 
 export default function App() {
   return <BrowserRouter>
