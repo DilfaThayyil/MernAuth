34eb322426355c2525227e80b54df8c23bdfcf98
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-27 17:44:21 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,8 +5,9 @@ export default function Header() {
   return (
     <div className='bg-slate-200'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-            
-            <h1 className='font-bold'>Auth App</h1>
+            <Link to='/'>
+                <h1 className='font-bold'>Auth App</h1>
+            </Link>
             <ul className='flex gap-4'>
                 <Link to='/'>
                     <li>Home</li>
