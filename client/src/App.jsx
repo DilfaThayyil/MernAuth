54dee91be7b4a65d639b8268ba37bbb5b5ecea3a
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-03 12:51:28 +0530

    added route setup for admin side pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
-import Header from './components/Header'
-import PrivateRoute from './components/PrivateRoute'
+import React from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
 
+const ConditionalHeader = () => {
+  const location = useLocation();
+  
+  const isAdminRoute = location.pathname.startsWith('/admin');
+  
+  return !isAdminRoute ? <Header /> : null;
+};
 
 export default function App() {
-  return <BrowserRouter>
-  <Header/>
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/sign-in' element={<SignIn/>}/>
-    <Route path='/sign-up' element={<SignUp/>}/>
-    <Route element={<PrivateRoute />}>
-      <Route path='/profile' element={<Profile />} />
-    </Route>
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <ConditionalHeader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/admin">
+          <Route path="login" element={<AdminLogin />} />
+          <Route path="" element={<AdminDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-
-
