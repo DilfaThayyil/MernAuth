563255bfcde2262696242e120388d83244f34ff8
DilfaThayyil <dilfathayyil@gmail.com>
2024-10-07 21:09:48 +0530

    lit changes made

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {GoogleAuthProvider,signInWithPopup,getAuth} from 'firebase/auth'
 import { app } from '../firebase';
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice'
 import { useNavigate } from 'react-router-dom'
 
